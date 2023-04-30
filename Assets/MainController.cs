a1c2cf75f5f69e10235a7c5577393eba98e7b5d5
Taylor Redden <tjredden1@gmail.com>
2023-04-29 19:38:15 -0700

    base launcher code

--- Assets/MainController.cs
+++ Assets/MainController.cs
@@ -8,10 +8,18 @@ public class MainController : MonoBehaviour
     public double bpm;
     public GameObject baby;
     private double lastTimeToNextBeat;
+    public List<GameObject> spawnPoints;
+    public List<GameObject> launcherPoints;
+    
+    private double timeToNextBeat;
+    private double beats;
+    private int currBeat;
     // Start is called before the first frame update
     void Start()
     {
         track = GetComponent<AudioSource>();
+        lastTimeToNextBeat = 0;
+        currBeat = -1;
     }
 
     // Update is called once per frame
@@ -20,17 +28,33 @@ public class MainController : MonoBehaviour
         int currentSample = track.timeSamples;
         double sampleRate = track.clip.frequency;
         double timeInSeconds = ((double)currentSample) / sampleRate;
-        double beats = timeInSeconds * bpm / 60.0;
-        double timeToNextBeat = Mathf.Ceil((float)(beats)) - beats;
+        beats = timeInSeconds * bpm / 60.0;
+        timeToNextBeat = Mathf.Ceil((float)(beats)) - beats;
 
-        Color newColor = baby.GetComponent<SpriteRenderer>().color;
-        newColor.a = Mathf.Pow((float)timeToNextBeat, 3);
-        baby.GetComponent<SpriteRenderer>().color = newColor;
-
-        
         if (lastTimeToNextBeat < timeToNextBeat) {
-            Debug.Log("Beat");
+            Debug.Log(currBeat);
+            currBeat++;
+            if (currBeat%4 == 0){
+                Spawnbaby();
+            }
         }
         lastTimeToNextBeat = timeToNextBeat;
+        
+    }
+
+    void Spawnbaby(){
+        int bag = Random.Range(0,3);// 9 total
+        GameObject newbaby = Instantiate(baby, spawnPoints[bag%3].transform.position, Quaternion.identity);
+        newbaby.GetComponent<BabyController>().bag = bag;
+        //Instantiate(baby, midSpawnPoint.transform);
+        //Instantiate(baby, rightSpawnPoint.transform);
+        
+    }
+    
+    public double GetNextBeat(){
+        return timeToNextBeat;
+    }
+    public double GetBeat(){
+        return beats;
     }
 }
