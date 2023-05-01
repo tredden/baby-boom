a48f707c67c6dc622b7228c984a2b248127951ea
Matthew Soulanille <matthew@soulanille.net>
2023-04-30 18:58:31 -0700

    Global variable for setting the song

--- Assets/Scenes/Routing.cs
+++ Assets/Scenes/Routing.cs
@@ -4,20 +4,63 @@ using UnityEngine;
 
 public class Routing : MonoBehaviour
 {
-    public void ShowSongsScene() {
+    public AudioClip song1Audio;
+    public AudioClip song2Audio;
+    public AudioClip song3Audio;
+
+    public void ShowSongsScene()
+    {
         // Show the scene named "Songs"
         UnityEngine.SceneManagement.SceneManager.LoadScene("Songs");
     }
-    public void ShowMenuScene() {
+    public void ShowMenuScene()
+    {
         // Show the scene named "Menu"
         UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
     }
-    public void ShowResultsScene() {
+    public void ShowResultsScene()
+    {
         // Show the scene named "Results"
         UnityEngine.SceneManagement.SceneManager.LoadScene("Results");
     }
-    public void ShowGameScene() {
+    private void ShowGameScene()
+    {
         // Show the scene named "Game"
         UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
+    private void playSong(SongConfig song)
+    {
+        // Set the song on the global variable holder
+        GameObject globalVariableHolder = GameObject.Find("GlobalVariableHolder");
+        globalVariableHolder.GetComponent<GlobalVariableHolder>().song = song;
+
+        // Show the game scene
+        ShowGameScene();
+    }
+    public void song1()
+    {
+        // Set the song config
+        SongConfig song = new SongConfig
+        {
+            audioClip = song1Audio,
+            bpm = 75,
+            columns = 1,
+            rows = 3,
+            spawnBabyPeriod = 2,
+        };
+        playSong(song);
+    }
+    public void song2()
+    {
+        // Set the song config
+        SongConfig song = new SongConfig
+        {
+            audioClip = song2Audio,
+            bpm = 240,
+            columns = 1,
+            rows = 3,
+            spawnBabyPeriod = 1,
+        };
+        playSong(song);
     }
 }
